55f3544412d0f492759f0e5844e3d9b06650115e
biropatrik <biropatrik9612@gmail.com>
2024-02-14 19:46:17 +0100

    [Feature] Init project and microservices

---
+++ microservices/shipper-service/src/main/java/hu/cubix/shipperservice/patrik/ShipperServiceApplication.java
@@ -0,0 +1,13 @@
+package hu.cubix.shipperservice.patrik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipperServiceApplication.class, args);
+	}
+
+}
---
+++ webshop/src/test/java/hu/cubix/webshop/patrik/WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.cubix.webshop.patrik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
