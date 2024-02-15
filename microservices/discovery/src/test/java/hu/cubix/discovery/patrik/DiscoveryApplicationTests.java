58dbe4fdd30ad8bd465c5e30ef746eb2daf1e114
biropatrik <biropatrik9612@gmail.com>
2024-02-15 22:18:58 +0100

    [Feature] discovery server added

---
+++ microservices/discovery/src/test/java/hu/cubix/discovery/patrik/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.cubix.discovery.patrik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
